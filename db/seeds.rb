9f7cc3114ca5796173c06cec936cfe2bb338df50
Taljjaa <jenken131@gmail.com>
2019-10-22 10:57:17 -0700

    rake now working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
-base_url = https://kitsu.io/api/edge/anime
+base_url = "https://kitsu.io/api/edge/anime"
 
 response_chihaya = RestClient.get("https://kitsu.io/api/edge/anime?filter[text]=chihayafuru")
+
+converted_to_json = JSON.parse(response_chihaya)
 
 binding.pry
 0
