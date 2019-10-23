c82ca745475547b0b91a57b38d0571536e75ea4d
Taljjaa <jenken131@gmail.com>
2019-10-22 17:05:19 -0700

    fully seeded data and started main menu

--- bin/run.rb
+++ bin/run.rb
@@ -2,9 +2,19 @@ require_relative '../config/environment'
 
 puts "----------WELCOME TO ANIME PICKER!----------"
 
+def main_menu
+    puts "Please create a username"
+    username = gets.chomp
+    User.find_or_create_by(username: username)
+    puts "Welcome, #{username}"
+    puts "What would you like to do"
+    puts "1. See my list of my animes"
+    puts "2. Highest rated anime"
+    puts "3. Add an anime to my list"
+    puts "4. See my stats"
+    puts "5. Exit =("
+    input = gets.chomp
+    main_menu
+end
 
 
-puts "Please create a username"
-username = gets.chomp
-User.find_or_create_by(username: username)
-puts "Welcome, #{username}"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,21 @@
-base_url = "https://kitsu.io/api/edge/anime?filter[text]="
+def create_seedling(url)
+    response = RestClient.get(url)
+    json_data = JSON.parse(response)["data"][0]
+    title = json_data["attributes"]["titles"]["en_us"]
+    avg_count = json_data["attributes"]["averageRating"].to_f
+    date = json_data["attributes"]["startDate"]
+    count = json_data["attributes"]["episodeCount"]
+    api_id = json_data["id"].to_i
+    Anime.find_or_create_by(title: title, average_rating: avg_count, start_date: date, episode_count: count, api_id: api_id)
+end
 
-response_chihaya = RestClient.get("https://kitsu.io/api/edge/anime?filter[text]=chihayafuru")
-
-json_data_chi = JSON.parse(response_chihaya)["data"][0]
-
-title = json_data_chi["attributes"]["titles"]["en"]
-avg_count = json_data_chi["attributes"]["averageRating"].to_f
-date = json_data_chi["attributes"]["startDate"]
-count = json_data_chi["attributes"]["episodeCount"]
-api_id = json_data_chi["id"].to_i
-
-
-#all my info is in converted_to_json["data"][0]
-
-Anime.create(title: title, average_rating: avg_count, start_date: date, episode_count: count, api_id: api_id)
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=steins%20gate")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=hunter%20x%20hunter")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=attack%20on%20titan")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=Ginga%20Eiyuu%20Densetsu")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=gintama")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=3%20gatsu%20no%20Lion%202nd%20Season")
+create_seedling("https://kitsu.io/api/edge/anime?filter[text]=Made%20in%20Abyss")
 
 binding.pry
 0
