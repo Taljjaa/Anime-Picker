0bf33a1bcf8ead6c50dd1b71bb260ab551d19ce5
Taljjaa <jenken131@gmail.com>
2019-10-22 20:04:35 -0700

    added inputs

--- bin/run.rb
+++ bin/run.rb
@@ -1,12 +1,14 @@
 require_relative '../config/environment'
+require 'pry'
 
 puts "----------WELCOME TO ANIME PICKER!----------"
 
+puts "Please create a username"
+$username = gets.chomp
+
 def main_menu
-    puts "Please create a username"
-    username = gets.chomp
-    User.find_or_create_by(username: username)
-    puts "Welcome, #{username}"
+    User.find_or_create_by(username: $username)
+    puts "Welcome, #{$username}"
     puts "What would you like to do"
     puts "1. See my list of my animes"
     puts "2. Highest rated anime"
@@ -14,7 +16,35 @@ def main_menu
     puts "4. See my stats"
     puts "5. Exit =("
     input = gets.chomp
+
+    if input == "1" 
+        if User.find_by(username: $username).animes.length < 1
+            puts "No animes found =("
+            puts "----------------------------------"
+        else 
+            puts User.find_by(username: $username).animes
+        end
+    end
+
+    if input == "2"
+        puts "potato 2"
+    end
+
+    if input == "3"
+        puts "pizza 3"
+    end
+
+    if input == "4"
+        puts "pasta 4"
+    end
+
+    if input == "5"
+        puts "Goodbye #{$username}"
+        return
+    end
     main_menu
 end
+
+main_menu
 
 
